c10e7b8a410d770441847602c6ef76856922f773
Afaz Khatri <afaz.khatri@bacancy.com>
2023-08-24 13:38:20 +0530

    Fix for google referrer

--- tests/referrer/matchers/search/google.test.js
+++ tests/referrer/matchers/search/google.test.js
@@ -7,19 +7,13 @@ test('parses false if not google referrer', () => {
 });
 
 test('parses google referrer from /search endpoint', () => {
-    const result = google(new URL('https://example.com'), new URL('https://google.com/search?q=example'))
+    const result = google(new URL('https://example.com'), new URL('https://google.com'))
 
-    expect(result).toMatchObject({ type: 'search', engine: 'google', query: 'example' });
+    expect(result).toMatchObject({ type: 'search', engine: 'google' });
 });
 
 test('parses google referrer from /url endpoint', () => {
     const result = google(new URL('https://example.com'), new URL('https://google.com/url?q=example'))
 
-    expect(result).toMatchObject({ type: 'search', engine: 'google', query: 'example' });
-});
-
-test('parses false google referrer when directly loading', () => {
-    const result = google(new URL('https://example.com'), new URL('https://google.com'))
-
-    expect(result).toBe(false);
+    expect(result).toMatchObject({ type: 'search', engine: 'google' });
 });
