a59d84444087ad0b8a7696941a5126a3eedcea17
Afaz Khatri <afaz.khatri@bacancy.com>
2023-08-28 12:50:34 +0530

    Added aol parser

---
+++ src/lib/referrer-parser/matchers/search/aol.js
@@ -0,0 +1,13 @@
+export default function (url, referrer) {
+    if (
+        referrer.host && referrer.href &&
+        referrer.host.indexOf('search.aol.com') !== -1
+    ) {
+      const description = { type: 'search', engine: 'aol' };
+      const query = referrer.searchParams.get('q');
+      if (query) description.query = query;
+      return description;
+    }
+    
+    return false;
+}
---
+++ tests/referrer/matchers/search/aol.test.js
@@ -0,0 +1,13 @@
+import aol from "../../../../src/lib/referrer-parser/matchers/search/aol";
+
+test('parses false if not aol referrer', () => {
+    const result = aol(new URL('https://example.com'), new URL('https://unknown-website.com'))
+
+    expect(result).toBe(false);
+});
+
+test('parses aol referrer', () => {
+    const result = aol(new URL('https://example.com'), new URL('https://search.aol.com'))
+
+    expect(result).toMatchObject({ type: 'search', engine: 'aol' });
+});
