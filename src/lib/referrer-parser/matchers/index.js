c593ee4ad7f3a7ed047c0534f6eb891fd7598538
Afaz Khatri <afaz.khatri@bacancy.com>
2023-09-04 18:38:18 +0530

    Added all the matchers

--- src/lib/referrer-parser/matchers/index.js
+++ src/lib/referrer-parser/matchers/index.js
@@ -1,5 +1,15 @@
+import aol from './search/aol';
+import baidu from './search/baidu';
+import bing from './search/bing';
+import duckduckgo from './search/duckduckgo';
 import google from './search/google';
+import yahoo from './search/yahoo';
 
 export default [
+    aol,
+    baidu,
+    bing,
+    duckduckgo,
     google,
+    yahoo
 ];
